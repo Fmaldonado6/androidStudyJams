cd7a57f4ec4ce60fed5ebc454949cdf5a4b287ba
Fernando Maldonado <fmaldonado824@gmail.com>
2021-05-21 19:30:10 -0500

    Navegación

---
+++ Practica3/app/src/main/java/com/fmaldonado/listas/DetalleActivity.kt
@@ -0,0 +1,11 @@
+package com.fmaldonado.listas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle)
+    }
+}
